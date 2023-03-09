992acfd73a54bbc41c28654aa54d4c70bba60615
AlessandroLF <lombardoalessandro25@hotmail.com>
2023-03-08 19:28:30 -0600

    base

---
+++ index.js
@@ -0,0 +1,52 @@
+const https = require("https");
+const path = require("path");
+const fs = require("fs");
+const TOKENF = "110010197565633.2-CMfFMD4V20qgG0ITHhUxZssFh2ae0xnZHGurD0Id";
+const TOKEN = "101800225595154.ccZtXqrc68FxVESWfrvLJZR0DAoiFCG";
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode');
+
+module.exports = (res) => {
+    fs.readFile(path.join(__dirname, "start.html"), (err, content) =>{
+        res.write(content);
+        res.write("<title>TestBed</title></head><body><h1>Testing</h1><div id='map'>t1</div>");
+        const client = new Client();
+        console.log('test');
+        client.on('qr', (qr) => {
+            // Generate and display QR code for user to scan
+            console.log('holi');
+            qrcode.toDataURL(qr.toString(), (err, url)=>{
+                res.write("<img src='" + url + "' >");
+            });
+          });
+          
+          client.on('ready', () => {
+            console.log('Client is ready!');
+          });
+
+          client.initialize();
+        /*
+        const options = {
+            hostname: 'web.whatsapp.com',
+            method: 'GET'
+            
+        };
+        console.log('pre');
+        const req = https.request(options, response => {
+            console.log(`statusCode: ${JSON(response)}`);
+            response.on('data', (chunk) =>{
+                console.log(chunk.toString());
+                res.write(chunk.toString());
+            });
+            response.on('end', () =>{
+                res.end();
+            });
+        });
+        req.on('error', e => {
+            console.log("error: " + e);
+        });
+        req.end();
+        */
+        
+    });
+};
