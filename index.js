837f9c4a24d4b6d8bb07e3fd55e67261561adf83
AlessandroLF <lombardoalessandro25@hotmail.com>
2023-03-08 21:07:16 -0600

    base

--- index.js
+++ index.js
@@ -7,15 +7,9 @@ module.exports = (res) => {
     fs.readFile(path.join(__dirname, "start.html"), (err, content) =>{
         res.write(content);
         res.write("<title>TestBed</title></head><body><h1>Testing</h1><div id='map'>t1</div>");
-        res.write('<div>test012</div>');
         const client = new Client();
-        qrcode.toDataURL("Testeando ando", (err, url)=>{
-            res.write("<img src='" + url + "' >");
-        });
-        res.write('<div>test</div>');
         client.on('qr', (qr, err) => {
             // Generate and display QR code for user to scan
-            res.write('<div>testmas'+ qr +'</div>');
             qrcode.toDataURL(qr, (err, url)=>{
                 res.write("<img src='" + url + "' >");
             });
