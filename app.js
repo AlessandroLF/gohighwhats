a642d9c5d6cf780daca17bdd20766b5dfd0ceced
AlessandroLF <lombardoalessandro25@hotmail.com>
2023-03-08 19:07:33 -0600

    base

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log ('hello');
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": contentType });
+    res.write("Hello");
+    res.end(content, "utf8");
+});
+  
+const PORT = process.env.PORT || 5000;
+  
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
